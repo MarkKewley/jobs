a4d7a870ae150064c27ea23419fa77bf9532f2e9
Kewley, Mark <mark.kewley@windlogics.com>
2018-01-03 17:20:43 -0600

    Initial MapView loaded

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,45 @@
+import { MapView } from 'expo';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      // controls center
+      longitude: -122,
+      latitude: 37,
+      // controls zoom level
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    },
+    mapLoaded: false
+  };
+
+  componentDidMount () {
+    this.setState({mapLoaded: true});
+  }
+
+  onRegionChangeComplete = region => {
+    this.setState({region});
+  };
 
   render () {
+    // for android, component needs to mount before loading MapView
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator size='large'/>
+        </View>
+      );
+    }
+
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{flex: 1}}>
+        <MapView
+          style={{flex: 1}}
+          region={this.state.region}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
       </View>
     );
   }
