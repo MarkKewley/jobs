6db99b3cfb2b1e762989f23b4055271f23ec28a5
Kewley, Mark <mark.kewley@windlogics.com>
2018-01-04 16:59:10 -0600

    added in fetch of jobs and jobs reducer

---
+++ actions/job_actions.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
+
+import { FETCH_JOBS } from './types';
+
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1, // return lat's and longs
+  radius: 10, // in miles
+  q: 'javascript' // the job search term, TODO: Add in a text input for searching for jobs
+};
+
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch';
+
+const buildJobsUrl = zip => {
+  // l => location. use postal code or a "city, state/province/region" combination
+  const query = qs.stringify({...JOB_QUERY_PARAMS, l: zip});
+  return `${JOB_ROOT_URL}?${query}`;
+};
+
+export const fetchJobs = region => async dispatch => {
+  try {
+    let zip = await reverseGeocode(region);
+    const url = buildJobsUrl(zip);
+    let {data} = await axios.get(url);
+    dispatch({type: FETCH_JOBS, payload: data});
+  } catch (e) {
+    console.error(e);
+  }
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import { MapView } from 'expo';
 import React, { Component } from 'react';
 import { ActivityIndicator, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -23,6 +26,10 @@ class MapScreen extends Component {
     this.setState({region});
   };
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  };
+
   render () {
     // for android, component needs to mount before loading MapView
     if (!this.state.mapLoaded) {
@@ -40,10 +47,29 @@ class MapScreen extends Component {
           region={this.state.region}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title='Search This Area'
+            backgroundColor={'#009688'}
+            icon={{name: 'search'}}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+
+  }
+};
+
+export default connect(null, actions)(MapScreen);
